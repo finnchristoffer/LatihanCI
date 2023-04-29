12bed6740160b287dfce4c9bbb314d0a1f6aa6d3
finnchristoffer <finn.christoffer@gmail.com>
2023-04-29 23:01:44 +0700

    Initial Commit

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by Finn Christoffer Kurniawan on 29/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
